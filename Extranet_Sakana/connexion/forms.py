0a87d6d1d84c50b061ba0e2ac154e8202da2a07d
JacquesHoschtettler <jacques.hoschtettler@gmx.com>
2020-08-12 16:24:18 +0200

    Majeure partie de la mise en forme de la page d'accueil faite.

--- Extranet_Sakana/connexion/forms.py
+++ Extranet_Sakana/connexion/forms.py
@@ -4,4 +4,3 @@ from django import forms
 class ConnexionForm(forms.Form):
     username = forms.CharField(max_length=30, label="Nom d'utilisateur")
     password = forms.CharField(widget=forms.PasswordInput, label="Mot de passe")
-
--- Extranet_Sakana/connexion/views.py
+++ Extranet_Sakana/connexion/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+# from django.contrib.auth.models import User
 
 from .forms import ConnexionForm
 
 
 def connexion(request):
+    error = False
     if request.method == "POST":
         form = ConnexionForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+            else:
+                error = True
     else:
         form = ConnexionForm()
-    return render(request, 'connexion/connexion.html')
-
-
+    return render(request, 'connexion/connexion.html', locals())
